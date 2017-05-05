8665440355571d512f9f0daa67736d74f1836fe0
Joel Thoman <jethoman@gmail.com>
2017-05-05 01:23:51 -0400

    joined item and order with foreign key

--- database/seeds/OrdersTableSeeder.php
+++ database/seeds/OrdersTableSeeder.php
@@ -5,16 +5,23 @@ use App\Order;
 class OrdersTableSeeder extends Seeder
 {
     /**
+    *Need to add an array with complete orders.
+    *
+    */
+    /**
      * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
+        $item_id = Item::where('item_desc', '=', $item_desc)->pluck('id')->first();
+
         Order::insert([
         'created_at' => Carbon\Carbon::now()->toDateTimeString(),
         'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
-        'order_desc' => 'Parts to fix computers'
+        'order_desc' => 'Parts to fix computers',
+        'item_id' => $item_id
         ]);
 
         Order::insert([
